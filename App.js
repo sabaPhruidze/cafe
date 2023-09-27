cc260dcc6becc5335642d966e31516ab77eee9f6
Saba Phruidze <sabaphruidze01@gmail.com>
2023-09-27 21:59:19 +0400

    I am continued

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, View } from "react-native";
 import { ContainerApp } from "./components/Style/ContainerApp";
 import { BGI } from "./components/Style/BGI";
+import { AvatarImg } from "./components/Style/AvatarImg";
+import styled from "styled-components";
 
 export default function App() {
   return (
     <ContainerApp>
       <StatusBar style="dark" />
       <BGI source={require("./assets/images/beansBackground1.png")} />
+      <SafeAreaViewApp>
+        <View>
+          <AvatarImg source={require("./assets/images/avatar.png")} />
+          <View></View>
+        </View>
+      </SafeAreaViewApp>
     </ContainerApp>
   );
 }
+
+const SafeAreaViewApp = styled.SafeAreaView`
+  display: flex;
+  flex: 1;
+`;
+const AvatarDiv = styled.Div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
---
+++ components/Style/AvatarImg.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+export const AvatarImg = styled.Image`
+  width: 36px;
+  height: 36px;
+  border-radius: 18px;
+`;
